595259e9d20013a73dfe655b2dd94d7f63cd1080
Marcin Kass <marcin.kass10@gmail.com>
2018-02-11 18:07:25 +0100

    create Create-view, and some update in controllers

--- WebTest/Controllers/CreateController.cs
+++ WebTest/Controllers/CreateController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebTest.Models;
 
 namespace WebTest.Controllers
 {
     public class CreateController : Controller
     {
         // GET: Create
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ WebTest/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebTest.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać swoje imię i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę podać swoj adres e-mail")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Proszę podać prawidłowy adres")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać swoj numer telefonu")]
+        public string Phone { get; set; }
+        
+        [Required(ErrorMessage = "Proszę określić czy weźmiesz udział")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
